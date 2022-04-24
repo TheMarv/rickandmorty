e66c18959403455fafdef8889ae2604868b84d04
Marvin Steckel <themarvdoteu@gmail.com>
2022-04-24 19:54:35 +0200

    Add location detail page

---
+++ scripts/locationDetails.js
@@ -0,0 +1,47 @@
+const locationDetailContainer = document.querySelector(
+  '[data-js="locationdetails"]'
+);
+const locationDetailName = locationDetailContainer.querySelector('h2');
+const locationDetailType = locationDetailContainer.querySelector(
+  '[data-js="locationType"]'
+);
+const locationDetailDimension = locationDetailContainer.querySelector(
+  '[data-js="locationDimension"]'
+);
+const locationDetailResidents = document.querySelector(
+  '[data-js="residentList"]'
+);
+
+export function fetchLocationDetails(id = 1) {
+  fetch(`https://rickandmortyapi.com/api/location/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      locationDetailName.textContent = data.name;
+      locationDetailType.textContent = `Type: ${data.type}`;
+      locationDetailDimension.textContent = `Dimension: ${data.dimension}`;
+      data.residents.slice(0, 5).forEach(residentURL => {
+        fetchCharacterDetails(residentURL).then(resident => {
+          const characterCard = document.createElement('div');
+          characterCard.className = 'charactercard';
+          characterCard.innerHTML = `
+            <img src="${resident.image}" alt="${resident.name}" />
+            <div class="charactercard__details">
+              <h2>${resident.name}</h2>
+              <p>${resident.name} is a ${resident.gender} ${resident.species}. ${resident.name}'s current status is ${resident.status}</p>
+            </div>
+          `;
+          locationDetailResidents.append(characterCard);
+        });
+      });
+    });
+}
+
+function fetchCharacterDetails(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    });
+}
+
+fetchLocationDetails();
