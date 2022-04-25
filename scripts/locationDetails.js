7ac85929ab7fe33679cbe81689114522e8ece1d3
Marvin Steckel <themarvdoteu@gmail.com>
2022-04-25 10:13:28 +0200

    Fix loading location details

--- scripts/locationDetails.js
+++ scripts/locationDetails.js
@@ -16,7 +16,7 @@ export function fetchLocationDetails(id = 1) {
   fetch(`https://rickandmortyapi.com/api/location/${id}`)
     .then(response => response.json())
     .then(data => {
-      locationDetailContainer.innerHTML = '';
+      locationDetailResidents.innerHTML = '';
       locationDetailName.textContent = data.name;
       locationDetailType.textContent = `Type: ${data.type}`;
       locationDetailDimension.textContent = `Dimension: ${data.dimension}`;
